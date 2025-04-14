fc1cc0b0e3f1c7a81deff35127a9c9bcd37deba6
Menahil ansari <menahilansari90@gmail.com>
2025-04-14 14:11:11 +0500

    my app completed

---
+++ App.js
@@ -0,0 +1,22 @@
+import logo from './log.svg';
+import './App.css';
+import {Header} from './Header';
+import Footer from './Footer';
+
+function App(){
+    return(
+        <div className="main">
+            <Header/>
+            <Container fluid>
+                <Container>
+                    
+                </Container>
+
+            </Container>
+
+            <Footer/>
+
+        </div>
+    );
+
+}
