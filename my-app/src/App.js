92ccb775cf90d3299d186991260dc5f713f95eb8
Menahil ansari <menahilansari90@gmail.com>
2025-04-15 15:50:13 +0500

    Header.css & Header.jsx added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from'./Header';
 
 function App() { 
   return (
     <div className="App">
+      <Header/>
     <h3 className='text-{40px} text-red-900 font-bold'>Welcome to WS</h3>
     </div>
   );
