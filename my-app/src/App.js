9e55c9d755dc0bde14d2481c02df187af7ea8bd3
Menahil ansari <menahilansari90@gmail.com>
2025-04-16 15:08:32 +0500

    ternary Operator added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,37 +3,23 @@ import './App.css';
 import Header from './Header';
 
 function App(){
-  let template='';
-  let{count,setCount}=useState(1)
-  let{pshow,setpshow}=useState(false)
-
-  if(pshow){
-    template=<>
-    <button>Hide</button>
-    <p>Welcome to WS</p>
-    </>
-  }
-  else{
-    template=<button className='bg-{red} p-4'>Show</button>
-  }
-
-  let displayData=()=>{
-    setCount(count+1)
-    
-  }
-  let addData=(a,b)=>{
-    console.log(a,b)
-  }
+  let[status,setStatus]=useState(false);
   return(
     <div className="App">
-      {template}
-      <div>
-        {count}
-      </div>
-      <button className='bg-[red] p-[10px] mr-4' onClick={()=>addData(20,25)}>Add data</button>
-      <button className='bg-[red] p-[10px]' onclick={displayData}>Save</button>
-      <Header/>
-      <h1 className='text-{40px} text-red-900 font-bold'>Welcome to WS</h1>
+      <button onClick={()=>setStatus(!status)}>
+        {(status)? 'Hide':'Show'}
+
+      </button>
+      {
+      (status)
+      ?
+     <p className='para'>Welcome to WS</p>
+     :
+     ''
+      }
+
     </div>
-  )
+  );
 }
+export default App;
+
