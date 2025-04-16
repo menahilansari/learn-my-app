d78ef50cb94aef8b97281b73ef1c85931a0691e9
Menahil ansari <menahilansari90@gmail.com>
2025-04-16 14:40:30 +0500

    if else condition added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,8 +3,22 @@ import './App.css';
 import Header from './Header';
 
 function App(){
-  let{count,setCount}=useState{1}
+  let template='';
+  let{count,setCount}=useState(1)
+  let{pshow,setpshow}=useState(false)
+
+  if(pshow){
+    template=<>
+    <button>Hide</button>
+    <p>Welcome to WS</p>
+    </>
+  }
+  else{
+    template=<button className='bg-{red} p-4'>Show</button>
+  }
+
   let displayData=()=>{
+    setCount(count+1)
     
   }
   let addData=(a,b)=>{
@@ -12,6 +26,10 @@ function App(){
   }
   return(
     <div className="App">
+      {template}
+      <div>
+        {count}
+      </div>
       <button className='bg-[red] p-[10px] mr-4' onClick={()=>addData(20,25)}>Add data</button>
       <button className='bg-[red] p-[10px]' onclick={displayData}>Save</button>
       <Header/>
