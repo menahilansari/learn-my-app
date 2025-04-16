8809536e2ccadf7c41f821d00e76c49397bb0cb1
Menahil ansari <menahilansari90@gmail.com>
2025-04-16 13:34:59 +0500

    changed app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from'./Header';
+import Header from './Header';
 
-function App() { 
-  return (
+function App(){
+  let{count,setCount}=useState{1}
+  let displayData=()=>{
+    
+  }
+  let addData=(a,b)=>{
+    console.log(a,b)
+  }
+  return(
     <div className="App">
+      <button className='bg-[red] p-[10px] mr-4' onClick={()=>addData(20,25)}>Add data</button>
+      <button className='bg-[red] p-[10px]' onclick={displayData}>Save</button>
       <Header/>
-    <h3 className='text-{40px} text-red-900 font-bold'>Welcome to WS</h3>
+      <h1 className='text-{40px} text-red-900 font-bold'>Welcome to WS</h1>
     </div>
-  );
+  )
 }
-export default App;
